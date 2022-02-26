8c28759523b23a3ed3b17244f5da58cd688724be
Janu Kleyn Fourie <s219295301@mandela.ac.za>
2022-02-26 19:19:58 +0200

    Add: User Controller

---
+++ ETutor-Api/Controllers/User/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETutor_Api.Controllers.User
+{
+    [Route("api/user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+    }
+}
